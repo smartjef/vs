52b5da16f271d5e6cfb14dd635993ef1086eaee8
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-06-09 18:36:50 +0300

    done with first step

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {
+        'title':'Blogs',
+        'blogs': Post.objects.filter(is_published=True),
+    }
+    return render(request, 'blog/index.html', context)
 
 def blog_detail(request, slug=None):
     return render(request, 'blog/details.html')
--- service/views.py
+++ service/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Service
 # Create your views here.
 def index(request):
-    return render(request, 'services/index.html')
+    context = {
+        'title': 'Our Services',
+        'services': Service.objects.all(),
+    }
+    return render(request, 'services/index.html', context)
 
 def service_details(request, slug=None):
     return render(request, 'services/details.html')
