86169abd383765442305d761a8a25dd3aa7fcd67
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-07-03 18:23:28 +0300

    added ai

--- ai/views.py
+++ ai/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def chat(request):
+    return render(request, 'ai/chat.html')
+
+def desc_to_image(request):
+    return render(request, 'ai/desc_to_image.html')
