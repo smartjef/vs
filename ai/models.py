7fc20991ec94e5259a07abf3388f04028b5d231f
Jeff Odhiambo <odhiambo.jeff1@students.jkuat.ac.ke>
2023-07-04 16:13:04 +0300

    AI desc to Image done

--- ai/models.py
+++ ai/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GeneratedImage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    image_url = models.URLField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.description}'
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Generated Image'
+        verbose_name_plural = 'Generated Images'
+
+class Trial(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='trials')
+    number = models.IntegerField(default=5)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.number}'
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Trial'
+        verbose_name_plural = 'Trials'
--- ai/urls.py
+++ ai/urls.py
@@ -5,4 +5,5 @@ app_name = 'ai'
 urlpatterns = [
     path('chat/', views.chat, name='chat'),
     path('desc-to-image/', views.desc_to_image, name='desc_to_image'),
+    path('desc-to-image/<int:id>/delete/', views.delete_generated_image, name='delete_generated_image'),
 ]
