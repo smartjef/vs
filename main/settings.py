1caaad5e1f5703b73d87adc64e7704207e135779
jeff <o.jeff3.a@gmail.com>
2024-10-14 14:29:52 +0300

    remove unused logos

--- main/settings.py
+++ main/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'core.middleware.LanguageMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
