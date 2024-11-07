e1020ecbb6f225d2c3569cb584d72655d1f2b7a5
jeff <o.jeff3.a@gmail.com>
2024-11-07 14:40:53 +0300

    update settings

--- core/views.py
+++ core/views.py
@@ -29,7 +29,6 @@ def index(request):
     }
     return render(request, f'index/{random_number}.html', context)
 
-@login_required
 def contact(request):
     if request.method == "POST":
         name = request.POST.get('full_name')
